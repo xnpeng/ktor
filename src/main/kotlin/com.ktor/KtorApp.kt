f254c73dbc51ab08613c9620f1e2b15722d9d2aa
xnpeng <xnpeng@hotmail.com>
2017-06-22 10:38:16 +0800

    when set static content folder,location class for get and set cannot be same one. why?

--- src/main/kotlin/com.ktor/KtorApp.kt
+++ src/main/kotlin/com.ktor/KtorApp.kt
@@ -1,19 +1,34 @@
 package com.ktor
 
 import com.ktor.handler.handler
+import com.ktor.model.User
 import freemarker.cache.ClassTemplateLoader
 import org.jetbrains.ktor.application.Application
+import org.jetbrains.ktor.application.ApplicationCall
+import org.jetbrains.ktor.application.feature
 import org.jetbrains.ktor.application.install
 import org.jetbrains.ktor.features.ConditionalHeaders
 import org.jetbrains.ktor.features.DefaultHeaders
 import org.jetbrains.ktor.features.PartialContentSupport
 import org.jetbrains.ktor.freemarker.FreeMarker
+import org.jetbrains.ktor.http.HttpHeaders
 import org.jetbrains.ktor.locations.Locations
 import org.jetbrains.ktor.logging.CallLogging
+import org.jetbrains.ktor.request.header
+import org.jetbrains.ktor.request.host
+import org.jetbrains.ktor.request.port
+import org.jetbrains.ktor.response.respondRedirect
 import org.jetbrains.ktor.routing.Routing
+import org.jetbrains.ktor.util.hex
+import java.net.URI
+import java.util.concurrent.TimeUnit
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 
 class KtorApp : AutoCloseable {
 
+    val hashKey = hex("6819b57a326945c1968f45236589")
+    val hmacKey = SecretKeySpec(hashKey, "HmacSHA1")
 
     fun Application.install() {
         install(DefaultHeaders)
@@ -29,6 +44,12 @@ class KtorApp : AutoCloseable {
             handler()
         }
 
+        fun hash(password: String): String {
+            val hmac = Mac.getInstance("HmacSHA1")
+            hmac.init(hmacKey)
+            return hex(hmac.doFinal(password.toByteArray(Charsets.UTF_8)))
+        }
+
         println("install completed.")
     }
 
@@ -36,3 +57,20 @@ class KtorApp : AutoCloseable {
     }
 
 }
+
+suspend fun ApplicationCall.redirect(location: Any) {
+    val host = request.host() ?: "localhost"
+    val portSpec = request.port().let { if (it == 80) "" else ":$it" }
+    val address = host + portSpec
+
+    respondRedirect("http://$address${application.feature(Locations).href(location)}")
+}
+
+fun ApplicationCall.securityCode(date: Long, user: User, hashFunction: (String) -> String) =
+        hashFunction("$date:${user.userId}:${request.host()}:${refererHost()}")
+
+fun ApplicationCall.verifyCode(date: Long, user: User, code: String, hashFunction: (String) -> String) =
+        securityCode(date, user, hashFunction) == code
+                && (System.currentTimeMillis() - date).let { it > 0 && it < TimeUnit.MILLISECONDS.convert(2, TimeUnit.HOURS) }
+
+fun ApplicationCall.refererHost() = request.header(HttpHeaders.Referrer)?.let { URI.create(it).host }
